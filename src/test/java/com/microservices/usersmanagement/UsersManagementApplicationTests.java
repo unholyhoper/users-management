673e6dba992e74001f0dcc98ef0f551d2befb48a
hbenyahia <hbenyahia@vermeg.com>
2023-04-19 04:24:43 +0100

    Initial commit

---
+++ src/test/java/com/microservices/usersmanagement/UsersManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.usersmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
